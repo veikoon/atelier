ee08dcc70174d11c24cf841a68d3a128ed608130
Vincent Monnot <vincent.monnot@edu.esiee.fr>
2020-01-24 14:24:45 +0100

    Ajout des textures de base

---
+++ main.py
@@ -0,0 +1,91 @@
+import pygame
+import random
+import copy
+ 
+#################################################################
+##
+##  variables du jeu 
+ 
+# 0 vide
+# 1 mur
+# 2 maison des fantomes (ils peuvent circuler mais pas pacman)
+
+TAB = [ [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+        [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+        [1,0,2,0,2,0,2,0,2,0,2,0,2,0,2,2,0,2,0,2,0,2,0,2,0,2,0,2,0,1],
+        [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+        [1,0,2,0,2,0,2,0,2,0,2,0,2,0,2,2,0,2,0,2,0,2,0,2,0,2,0,2,0,1],
+        [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+        [1,0,2,0,2,0,2,0,2,0,2,0,2,0,2,2,0,2,0,2,0,2,0,2,0,2,0,2,0,1],
+        [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+        [1,0,2,0,2,0,2,0,2,0,2,0,2,0,2,2,0,2,0,2,0,2,0,2,0,2,0,2,0,1],
+        [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+        [1,0,2,0,2,0,2,0,2,0,2,0,2,0,2,2,0,2,0,2,0,2,0,2,0,2,0,2,0,1],
+        [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+        [1,0,2,0,2,0,2,0,2,0,2,0,2,0,2,2,0,2,0,2,0,2,0,2,0,2,0,2,0,1],
+        [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+        [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]]
+
+# Define some colors
+BLACK = [0, 0, 0]
+WHITE = [255, 255, 255]
+GREEN = [0, 255, 0]
+RED   = [255, 0, 0]
+BLUE  = [0 , 0 , 255]
+CYAN  = [ 0, 255 ,255]
+ORANGE = [255, 165, 0 ]
+PINK = [ 255,105,180]
+YELLOW = [255,255,0]          
+
+   
+ZOOM = 64   # taille d'une case en pixels
+EPAISS = 8  # epaisseur des murs bleus en pixels
+HAUTEUR = len(TAB)     # nb de cases en hauteur
+LARGEUR = len(TAB[0])  # nb de cases en largeur
+
+Block = pygame.image.load("images/blocks/stone.png")
+BlockMiddle = pygame.image.load("images/blocks/stone2.png")
+#################################################################
+##
+##  INIT FENETRE 
+def Dessine():
+	for i in range(LARGEUR):
+		for j in range(HAUTEUR):
+			if(TAB[j][i] == 2):
+				screen.blit(BlockMiddle,(i*ZOOM,j*ZOOM))
+			if(TAB[j][i] == 1):
+				screen.blit(Block,(i*ZOOM,j*ZOOM))
+
+
+# Setup
+pygame.init()
+police = pygame.font.SysFont("arial", 22)
+screeenWidth = (LARGEUR+1) * ZOOM
+screenHeight = (HAUTEUR+2) * ZOOM
+screen = pygame.display.set_mode((screeenWidth,screenHeight))
+pygame.display.set_caption("ESIEE - BOMBERMAN")
+done = False
+clock = pygame.time.Clock()   
+pygame.mouse.set_visible(True) 
+
+#################################################################
+##
+##   GAME LOOP
+
+
+# -------- Main Program Loop -----------
+while not done:
+   event = pygame.event.Event(pygame.USEREVENT)    
+   pygame.event.pump()
+   for event in pygame.event.get():
+      if event.type == pygame.QUIT:
+         done = True
+     
+   pygame.display.flip()
+ 
+   Dessine()
+    # Limit frames per second
+   clock.tick(4)
+ 
+# Close the window and quit.
+pygame.quit()
