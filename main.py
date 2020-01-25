105239dee2872249a89361d3d05ef4238ae9b288
blackjack-nix <theo.peresse-gourbil@edu.esiee.fr>
2020-01-25 20:40:20 +0100

    pose de bombe

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 #################################################################################
-#	Class main from Bomberman													#
-#	Created by Vincent : 24/01/2020												#
-#																				#
-#	Cette classe est la classe principale du jeu, elle permet de definir le 	#
-#	le decor, et contient la boucle du jeu 										#
-#																				#
+#   Class main from Bomberman                                                   #
+#   Created by Vincent : 24/01/2020                                             #
+#                                                                               #
+#   Cette classe est la classe principale du jeu, elle permet de definir le     #
+#   le decor, et contient la boucle du jeu                                      #
+#                                                                               #
 #################################################################################
 
 #################################################################################
@@ -69,60 +69,72 @@ Jaune = pygame.image.load("images/ia/Jaune/sprite.png")
 Orange = pygame.image.load("images/ia/Orange/sprite.png")
 Vert = pygame.image.load("images/ia/Vert/sprite.png")
 
+#du sprite de la bombe
+Bombe = pygame.image.load("images/bombe/bomb.png")
+
 #################################################################################
 ##
 ##  Fonctions principales
 
 # getSprite(Color):
-# 	Decoupe l'image Color en sprite
-#	Met les sprite a l'echelle de la carte
-#	Les rajoute dans un tableau en 2D tel que :
-#	Tab = [[SpriteAvant_1, SpriteAvant_2, ...],[SpriteDroit_1, SpriteDroit_2, ...]]
+#   Decoupe l'image Color en sprite
+#   Met les sprite a l'echelle de la carte
+#   Les rajoute dans un tableau en 2D tel que :
+#   Tab = [[SpriteAvant_1, SpriteAvant_2, ...],[SpriteDroit_1, SpriteDroit_2, ...]]
 def getSprite(Color):
-	Tab = []
-	for j in range(4):
-		tabTemp = []
-		for i in range(4):
-			imTemp = Color.subsurface((i*29) + (3*i) + 3,0 + (j*48),29,46)
-			imTemp = pygame.transform.scale(imTemp,(64,102))
-			tabTemp.append(imTemp)
-		Tab.append(tabTemp)
-	return Tab
+    Tab = []
+    for j in range(4):
+        tabTemp = []
+        for i in range(4):
+            imTemp = Color.subsurface((i*29) + (3*i) + 3,0 + (j*48),29,46)
+            imTemp = pygame.transform.scale(imTemp,(64,102))
+            tabTemp.append(imTemp)
+        Tab.append(tabTemp)
+    return Tab
 
 # dessine():
-#	Parcourt TAB et place les images aux coordonnees idoines
-#	en fonction de la valeur des cases du tableau
-#	Puis place les joueurs
+#   Parcourt TAB et place les images aux coordonnees idoines
+#   en fonction de la valeur des cases du tableau
+#   Puis place les joueurs
 def dessine():
-	for i in range(LARGEUR):
-		for j in range(HAUTEUR):
-			if(TAB[j][i] == 3):
-				screen.blit(Brick,(i*ZOOM,j*ZOOM))
-			if(TAB[j][i] == 2):
-				screen.blit(BlockMiddle,(i*ZOOM,j*ZOOM))
-			if(TAB[j][i] == 1):
-				screen.blit(Block,(i*ZOOM,j*ZOOM))
-			if(TAB[j][i] == 0):
-				screen.blit(Grass,(i*ZOOM,j*ZOOM))
+    for i in range(LARGEUR):
+        for j in range(HAUTEUR):
+            if(TAB[j][i] == 4):
+                screen.blit(Bombe,(i*ZOOM,j*ZOOM))
+            if(TAB[j][i] == 3):
+                screen.blit(Brick,(i*ZOOM,j*ZOOM))
+            if(TAB[j][i] == 2):
+                screen.blit(BlockMiddle,(i*ZOOM,j*ZOOM))
+            if(TAB[j][i] == 1):
+                screen.blit(Block,(i*ZOOM,j*ZOOM))
+            if(TAB[j][i] == 0):
+                screen.blit(Grass,(i*ZOOM,j*ZOOM))
 
-	JoueurBleu.draw(screen)
-	screen.blit(font.render(str(actualTime // 1), True, WHITE), ((1920 // 2) - 25 , 64*HAUTEUR + 32))
-	pygame.display.flip() # Rafraichis l'affichage de Pygame
+    JoueurBleu.draw(screen)
+    screen.blit(font.render(str(actualTime // 1), True, WHITE), ((1920 // 2) - 25 , 64*HAUTEUR + 32))
+    pygame.display.flip() # Rafraichis l'affichage de Pygame
 
 ## move():
-#	On change les coordonnees du joueur selon son deplacement
-#	On regarde la retenu de sprite est complete ou non:
-#		* Si oui on change de sprite (+1 %Nombre de sprite pour ne pas sortir du tableau) et et on reset la retenu de sprite
-#		* Si non on augmente la retenu
-#	(permet d'eviter un changement de sprite trop rapide par rapport a sa vitesse)
+#   On change les coordonnees du joueur selon son deplacement
+#   On regarde la retenu de sprite est complete ou non:
+#       * Si oui on change de sprite (+1 %Nombre de sprite pour ne pas sortir du tableau) et et on reset la retenu de sprite
+#       * Si non on augmente la retenu
+#   (permet d'eviter un changement de sprite trop rapide par rapport a sa vitesse)
 def move(player, posX, posY):
-	player.y += posY
-	player.x += posX
-	if(player.spriteOffset == 2):
-		player.spriteCount = (JoueurBleu.spriteCount + 1) % 4
-		player.spriteOffset = 0
-	else:
-		player.spriteOffset += 1
+    player.y += posY
+    player.x += posX
+    if(player.spriteOffset == 2):
+        player.spriteCount = (player.spriteCount + 1) % 4
+        player.spriteOffset = 0
+    else:
+        player.spriteOffset += 1
+
+def poseBombe(player):
+    caseX = int(player.x/ZOOM)
+    caseY = int(player.y/ZOOM)
+    if(TAB[caseY][caseX] == 0):
+        TAB[caseY][caseX] = 4
+
 
 #################################################################################
 ##
@@ -153,34 +165,36 @@ ZOOM = int((64/1920)*screeenWidth)   # Taille d'une case en pixels
 
 # --------  Main -----------
 while not done:
-	event = pygame.event.Event(pygame.USEREVENT)
-	pygame.event.pump()
+    event = pygame.event.Event(pygame.USEREVENT)
+    pygame.event.pump()
 
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			done = True
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
 
-	keysPressed = pygame.key.get_pressed()	# On retient les touches pressees
+    keysPressed = pygame.key.get_pressed()  # On retient les touches pressees
 
-	## Mouvements du Joueur
-	#	On choisit la direction du sprite en fonction de sa position dans le tableau des sprites
-	#	On fait appelle a la fonction move pour changer les coordonnees et les sprites
-	if(keysPressed[pygame.K_DOWN]):
-		JoueurBleu.spriteDir = 0
-		move(JoueurBleu,0,4)
-	if(keysPressed[pygame.K_UP]):
-		move(JoueurBleu,0,-4)
-		JoueurBleu.spriteDir = 3
-	if(keysPressed[pygame.K_RIGHT]):
-		move(JoueurBleu,4,0)
-		JoueurBleu.spriteDir = 2
-	if(keysPressed[pygame.K_LEFT]):
-		move(JoueurBleu,-4,0)
-		JoueurBleu.spriteDir = 1
+    ## Mouvements du Joueur
+    #   On choisit la direction du sprite en fonction de sa position dans le tableau des sprites
+    #   On fait appelle a la fonction move pour changer les coordonnees et les sprites
+    if(keysPressed[pygame.K_DOWN]):
+        JoueurBleu.spriteDir = 0
+        move(JoueurBleu,0,4)
+    if(keysPressed[pygame.K_UP]):
+        move(JoueurBleu,0,-4)
+        JoueurBleu.spriteDir = 3
+    if(keysPressed[pygame.K_RIGHT]):
+        move(JoueurBleu,4,0)
+        JoueurBleu.spriteDir = 2
+    if(keysPressed[pygame.K_LEFT]):
+        move(JoueurBleu,-4,0)
+        JoueurBleu.spriteDir = 1
+    if(keysPressed[pygame.K_SPACE]):
+        poseBombe(JoueurBleu)
 
-	actualTime = time.time() - temps
-	screen.fill(BLACK)
-	dessine()	# On redessine l'affichage et on actualise
-	clock.tick(30) # Limite d'image par seconde
+    actualTime = time.time() - temps
+    screen.fill(BLACK)
+    dessine()   # On redessine l'affichage et on actualise
+    clock.tick(30) # Limite d'image par seconde
 
 pygame.quit() # Ferme la fenetre et quitte.
