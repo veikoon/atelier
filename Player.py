ec8a2f5081c830496f3203c661ec8d6f21b6d9ff
CappiLucky <manon.hermann@edu.esiee.fr>
2020-01-27 16:41:18 +0100

    ajout class IA

--- Player.py
+++ Player.py
@@ -30,7 +30,7 @@ class Player:
 	#   Met les sprite a l'echelle de la carte
 	#   Les rajoute dans un tableau en 2D tel que :
 	#   Tab = [[SpriteAvant_1, SpriteAvant_2, ...],[SpriteDroit_1, SpriteDroit_2, ...]]
-	def getSprite(self, Color, hauteur, zoom):	
+	def getSprite(self, Color, hauteur, zoom):
 		Tab = []
 		for j in range(4):
 			tabTemp = []
@@ -56,3 +56,26 @@ class Player:
 		else:
 			self.spriteOffset += 1
 
+
+
+#################################################################################
+#	Class Player from Bomberman													#
+#	Created by Manon : 27/01/2020												#
+#																				#
+#	Cette classe permet de definir toutes les propriete d'une IA				#
+#	Cette classe herite de la classe player										#
+#																				#
+#################################################################################
+
+
+class IA(Player):
+
+	def __init__(self,startX, startY, color, hauteur, zoom, direction):
+		self.x = startX				# Positions initiales
+		self.y = startY
+		self.sprite = self.getSprite(color, hauteur, zoom)	# Tableau de sprites en 2D
+		self.spriteDir = 0			# Selectionne le tableau de sprite (en 1D) correspondant a la direction du joueur
+		self.spriteCount = 0		# Selectionne le sprite du tableau correspondant au mouvement actuel
+		self.spriteOffset = 0		# Permet de changer de sprite en fonction du decalage et non a chaque mouvement
+		self.lives = 1
+		self.dir = direction
