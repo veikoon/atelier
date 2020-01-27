4b0586819cd5acff41a6739934855479ca995ff3
blackjack-nix <theo.peresse-gourbil@edu.esiee.fr>
2020-01-27 11:31:03 +0100

    bomb class doc

--- Bombe.py
+++ Bombe.py
@@ -1,9 +1,9 @@
 #################################################################################
-#	Class Bombe from Bomberman													#
-#	Created by Quentin : 25/01/2020												#
-#																				#
-#	Cette classe permet de definir toutes les propriete d'une Bombe			#
-#																				#
+#   Class Bombe from Bomberman                                                  #
+#   Created by Quentin : 25/01/2020                                             #
+#                                                                               #
+#   Cette classe permet de definir toutes les propriete d'une Bombe         #
+#                                                                               #
 #################################################################################
 import pygame
 from pygame import *
@@ -11,12 +11,12 @@ class Bombe:
     def __init__(self,startX, startY, spriteTab):
         self.explode = False
         self.game_frame = 0         # initialize the game_frame counter
-        self.x = startX				# Position initiale en x
+        self.x = startX             # Position initiale en x
         self.y = startY             # Position initiale en y
-        self.sprite = spriteTab		# Tableau de sprites en 2D
-        self.spriteDir = 0			# Selectionne le tableau de sprite (en 1D) correspondant a la direction du joueur
-        self.spriteCount = 0		# Selectionne le sprite du tableau correspondant au mouvement actuel
-        self.spriteOffset = 0		# Permet de changer de sprite en fonction du decalage et non a chaque mouvement
+        self.sprite = spriteTab     # Tableau de sprites en 2D
+        self.spriteDir = 0          # Selectionne le tableau de sprite (en 1D) correspondant a la direction du joueur
+        self.spriteCount = 0        # Selectionne le sprite du tableau correspondant au mouvement actuel
+        self.spriteOffset = 0       # Permet de changer de sprite en fonction du decalage et non a chaque mouvement
         self.ListBomb =[]
 
 
@@ -54,7 +54,7 @@ class Bombe:
 
 
 
-    ##	draw():
+    ##  draw():
     # Permet de dessiner le personnage dont les coordonnees sont au milieu de ses pieds
     # tandis que le jeu dessine les images depuis leur coin superieur gauche:
     # (largeurPerso / 2 = 32 et hauteurPerso = 102)
