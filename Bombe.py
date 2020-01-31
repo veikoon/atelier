1bc3e7d26b0e81191d3c54ce064603bd5e791459
CappiLucky <manon.hermann@edu.esiee.fr>
2020-01-31 11:40:20 +0100

    commentaire pour le fichier bombe

--- Bombe.py
+++ Bombe.py
@@ -1,40 +1,47 @@
 #################################################################################
+##
+## Import
+
+import pygame
+from pygame import *
+import time
+
+
+#################################################################################
 #   Class Bombe from Bomberman                                                  #
 #   Created by Quentin : 25/01/2020                                             #
 #                                                                               #
 #   Cette classe permet de definir toutes les propriete d'une Bombe             #
 #                                                                               #
 #################################################################################
-import pygame
-from pygame import *
-import time
 
 class Bombe:
     def __init__(self,startX, startY, bombes, zoom, times, player):
-        self.player = player
-        self.explode = False
-        self.rayon = player.rayonBombe  # initialize the game_frame counter
-        self.x = startX             # Position initiale en x
-        self.y = startY             # Position initiale en y
+        self.player = player                         # Association d'une bombe au joueur
+        self.explode = False                         # Explosion de la bombe
+        self.rayon = player.rayonBombe               # Initialize the game_frame counter
+        self.x = startX                              # Position initiale en x
+        self.y = startY                              # Position initiale en y
         self.sprite = self.getSprite(bombes, zoom)   # Tableau de sprites en 2D
-        self.spriteDir = 0    
-        self.timeBomb = times      # Selectionne le tableau de sprite (en 1D) correspondant a la direction du joueur
-        self.spriteCount = 0        # Selectionne le sprite du tableau correspondant au mouvement actuel
-        self.spriteOffset = 0       # Permet de changer de sprite en fonction du decalage et non a chaque mouvement
-        self.caseX = player.x // zoom
+        self.spriteDir = 0
+        self.timeBomb = times                        # Selectionne le tableau de sprite (en 1D) correspondant a la direction du joueur
+        self.spriteCount = 0                         # Selectionne le sprite du tableau correspondant au mouvement actuel
+        self.spriteOffset = 0                        # Permet de changer de sprite en fonction du decalage et non a chaque mouvement
+        self.caseX = player.x // zoom                # Savoir sur quelle case est la bombe
         self.caseY = player.y // zoom
-        self.exploFin = False
+        self.exploFin = False                        # Defini la fin de l'explosion de la bombe
         self.finexplode = False
         self.explX = False
         self.explY = False
         self.explmX = False
         self.explmY = False
 
-# getSpriteBombe(imgBombe):
-#   Decoupe l'image imgBombe en sprite
-#   Met les sprite a l'echelle de la carte
-#   Les rajoute dans un tableau en 2D tel que :
-#   Tab = [bombe1,bombe2...]
+
+    ## getSpriteBombe(self, imgBombe, zoom):
+    #   Decoupe l'image imgBombe en sprite
+    #   Met les sprite a l'echelle de la carte
+    #   Les rajoute dans un tableau en 2D tel que :
+    #   Tab = [bombe1,bombe2...]
     def getSprite(self, imgBombe, zoom):
         Tab = []
         tabTemp = []
@@ -45,20 +52,18 @@ class Bombe:
         Tab.append(tabTemp)
         return Tab
 
-    
-# Passe au sprite suivant en parcourant le tableau
+
+    ## anim(self, time):
+    #   Passe au sprite suivant en parcourant le tableau
     def anim(self,TIME):
         finexplode = False
         decompte =1
-        # print(TIME - self.timeBomb)
-        # print("verifi")
-        # print(1*self.spriteCount)
         if(self.finexplode):
             decompte = 0.4
-        
+
         if (TIME - self.timeBomb > decompte*self.spriteCount):
             self.spriteCount = (self.spriteCount + 1)
-            
+
         if(self.spriteCount == 4 and self.finexplode):
             self.exploFin = True
 
@@ -69,11 +74,8 @@ class Bombe:
             self.spriteCount = 0
 
 
-    # def animExplo(self):
-    #     TIME2 = time.time()    
-    #     if (TIME2 > 1):
-    #         self.spriteCountEx = (self.spriteCountEx + 1)
-    #         self.game_frame += 1
+    ## animExplo(self, imgfire, zoom):
+    #    Fonction qui affiche l'explosion
     def getSpriteExplo(self,ImgFire, zoom):
         Tab = []
         for j in range(3):
@@ -86,54 +88,53 @@ class Bombe:
         return Tab
 
 
-# Accesseur pour l'explosion
+    ## Explode(self):
+    #   Accesseur pour l'explosion
     def Explode(self):
         return self.explode
 
-    ##  draw():
-    # Permet de dessiner le personnage dont les coordonnees sont au milieu de ses pieds
-    # tandis que le jeu dessine les images depuis leur coin superieur gauche:
-    # (largeurPerso / 2 = 32 et hauteurPerso = 102)
+
+    ##  draw(self, surface):
+    #   Permet de dessiner le personnage dont les coordonnees sont au milieu de ses pieds
+    #   Tandis que le jeu dessine les images depuis leur coin superieur gauche:
+    #   (largeurPerso / 2 = 32 et hauteurPerso = 102)
     def draw(self, surface):
         surface.blit(self.sprite[self.spriteDir][self.spriteCount], (self.x - 100, self.y - 96))
 
-    def drawExplo(self, surface,TAB,listeBombe,i,direction, zoom):
 
-        HAUTEUR = len(TAB)     # Nombre de cases en hauteur
-        LARGEUR = len(TAB[0])  # Nombre de cases en largeur
-        
-        # Declanche le debu de l'explosion (branches)
-        if(self.finexplode == True) : # Timer de la bombe est fini
-            caseX = self.caseX        
+    ## drawExplo(self, surface, tab, listebombe, i, direction, zoom):
+    #   Dessine l'explosion de la bombe
+    #   On verifie les cases au alentour pour savoir jusqu'où va l'explosion
+    def drawExplo(self, surface,TAB,listeBombe,i,direction, zoom):
+        HAUTEUR = len(TAB)          # Nombre de cases en hauteur
+        LARGEUR = len(TAB[0])       # Nombre de cases en largeur
+
+        if(self.finexplode == True) : # Declanche le debut de l'explosion (branches)
+            caseX = self.caseX
             caseY = self.caseY
             explX = self.explX
             explY = self.explY
-            spriteDir = 1 #1 pour dessiner les branches
-      # permet de placer correctement la sprite
-            testcase =-1
-
-            rotatex=0
-            rotatey=270
-            
-            place = zoom*i+zoom
+            spriteDir = 1   # 1 pour dessiner les branches
+            testcase =-1    # Permet de placer correctement la sprite
+            rotatex=0       # Rotation de la sprite en x
+            rotatey=270     # Rotation de la sprite en y
+            place = zoom*i+zoom     # Place correctement la sprite de l'explosion
 
             if(self.rayon-1 == i):
-                spriteDir = 2 # bout du sprite
+                spriteDir = 2   # Bout de l'explosion
 
             if(direction < 0):
                 rotatex=180
                 rotatey=90
                 place = -place
-                testcase = 1 
+                testcase = 1
                 explX = self.explmX
                 explY = self.explmY
-            # Reinitialise la case de la bombe
-            if(self.exploFin):
+
+            if(self.exploFin):     # Reinitialise la case de la bombe
                 TAB[caseY][caseX] = 0
-            
-            
-            if(self.caseX >= LARGEUR-2 and direction >0):
-                b= 2
+
+            if(self.caseX >= LARGEUR-2 and direction >0): b= 2
             else:
                 caseTesteX = TAB[caseY][caseX+direction]
                 if(self.animStop(TAB,direction,testcase,'x',caseX,caseY) and (caseTesteX == 3 and not explX)):
@@ -142,22 +143,17 @@ class Bombe:
                             self.explX= True
                         else:
                             self.explmX= True
-                if(self.animGo(TAB,direction,testcase,'x',caseX,caseY)): # verifie si c'est une case bombable                    
-                    
+                if(self.animGo(TAB,direction,testcase,'x',caseX,caseY)): # verifie si c'est une case bombable
                     if(TAB[caseY][caseX+direction]==4):
-                        self.explMulti(listeBombe,direction,'x') 
+                        self.explMulti(listeBombe,direction,'x')
 
                     TAB[caseY][caseX+direction]=5
                     if(self.exploFin):
                         TAB[caseY][caseX+direction]=0
-                    rotateXimg =pygame.transform.rotate(self.sprite[spriteDir][self.spriteCount],rotatex)    
+                    rotateXimg =pygame.transform.rotate(self.sprite[spriteDir][self.spriteCount],rotatex)
                     surface.blit(rotateXimg, (self.x + place - 100 , self.y - 96))
-                      
-            
-            
-                    
-            if(self.caseY >= HAUTEUR-2 and direction >0):
-                a = 3
+
+            if(self.caseY >= HAUTEUR-2 and direction >0):   a = 3
             else:
                 caseTesteY = TAB[caseY+direction][caseX]
 
@@ -167,35 +163,47 @@ class Bombe:
                         self.explY= True
                     else:
                         self.explmY= True
-                
+
                 if(self.animGo(TAB,direction,testcase,'y',caseX,caseY)):
                     if(TAB[caseY+direction][caseX]==4):
-                        self.explMulti(listeBombe,direction,'y') 
+                        self.explMulti(listeBombe,direction,'y')
                     TAB[caseY+direction][caseX] = 5
 
                     if(self.exploFin):
                         TAB[caseY+direction][caseX] = 0
 
-                    rotateY =pygame.transform.rotate(self.sprite[spriteDir][self.spriteCount],rotatey)    
-                    surface.blit(rotateY, (self.x - 100 , self.y + place - 96))   
-        
+                    rotateY =pygame.transform.rotate(self.sprite[spriteDir][self.spriteCount],rotatey)
+                    surface.blit(rotateY, (self.x - 100 , self.y + place - 96))
+
+
+    ## animGo(self, tab, direction, testcase, sens, casex, casey):
+    #   Permet de savoir le sens de l'affichage de la sprite de l'explosion
+    #   On verifie la coherence avec les murs autour
     def animGo(self,TAB,direction,testcase,sens,caseX,caseY):
-        if(sens == 'y'):  
+        if(sens == 'y'):
             caseTeste = TAB[caseY+direction][caseX]
             caseTesteY = TAB[caseY+direction+testcase][caseX]
             return (((caseTeste == 0 and caseTesteY != 3)  or caseTeste == 5 or caseTeste == 4)and self.animStop(TAB,direction,testcase,sens,caseX,caseY))
-        if(sens == 'x'):  
+        if(sens == 'x'):
             caseTesteX = TAB[caseY][caseX+direction+testcase]
             caseTeste = TAB[caseY][caseX+direction]
             return (((caseTeste == 0 and caseTesteX != 3) or caseTeste == 5 or caseTeste == 4) and self.animStop(TAB,direction,testcase,sens,caseX,caseY))
-    
+
+
+    ## animStop(self, tab, directio, testcase, sens, casex, casey):
+    #   Permet de savoir quand la sprite doit s'arreter
+    #   Prend enn compte les murs autour et ce qui a deja ete detruit
     def animStop(self,TAB,direction,testcase,sens,caseX,caseY):
-        if(sens == 'x'): return TAB[caseY][caseX+direction+testcase]!=2 
-        if(sens == 'y'): return TAB[caseY+direction+testcase][caseX]!=2 
-    
+        if(sens == 'x'): return TAB[caseY][caseX+direction+testcase]!=2
+        if(sens == 'y'): return TAB[caseY+direction+testcase][caseX]!=2
+
+
+    ## explMulti(self, listebombe, direction, sens):
+    #   Permet de faire exploser une bombe qui est a cote d'une autre
+    #   Si une bombe exploser et qu'une autre bombe est dans son rayon d'explosion alors elle explose en meme temps
     def explMulti(self,listeBombe,direction,sens):
         for bombe in listeBombe:
             if(sens =='y'):
-                 if(bombe.caseX == self.caseX and bombe.caseY == self.caseY+direction ): bombe.timeBomb = 0  
+                 if(bombe.caseX == self.caseX and bombe.caseY == self.caseY+direction ): bombe.timeBomb = 0
             if(sens =='x'):
-                if(bombe.caseX == self.caseX +direction and bombe.caseY == self.caseY ): bombe.timeBomb = 0  
+                if(bombe.caseX == self.caseX +direction and bombe.caseY == self.caseY ): bombe.timeBomb = 0
